80683ec98e8cf67971253c5cb22469b2376fbf1c
Sigurd Fosseng <sigurd.fosseng@husbanken.no>
2015-04-14 07:50:43 +0200

    Legg på dato på tweet-meldinga, og vis dato på formatet "dd.MM H:mm:ss" i  tweet lista

--- klient/app/scripts/controllers/main.js
+++ klient/app/scripts/controllers/main.js
@@ -4,7 +4,7 @@ angular.module('klientApp')
   .controller('MainCtrl', function ($scope) {
     $scope.tweets = [];
     $scope.leggTilTweet = function(){
-      $scope.tweets.push($scope.tweet);
+      $scope.tweets.push({melding: $scope.tweet, dato: new Date()});
       $scope.tweet = "";
     };
   });
